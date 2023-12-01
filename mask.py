ff0b03ce2719fc10a297d055d47d3544938f5c23
kamil <kamiljelonek227@gmail.com>
2023-12-01 18:43:36 +0100

    extract mask

--- mask.py
+++ mask.py
@@ -1,63 +1,15 @@
 import cv2
 import mediapipe as mp
 
-# cascPath = "Scripts/haarcascade_frontalface_default.xml"
-# faceCascade = cv2.CascadeClassifier(cascPath)
-#
-# # Set video source to default webcam (at index 0)
-# video_capture = cv2.VideoCapture(0)
-#
-# # Check if the video capture is opened correctly
-# if not video_capture.isOpened():
-#     print("Error: Camera not accessible")
-#     exit()
-#
-# # Capture video
-# while True:
-#     ret, frame = video_capture.read()
-#
-#     # Check if frame is read correctly
-#     if not ret:
-#         print("Error: Frame not read properly")
-#         break
-#
-#     try:
-#         # Convert to grayscale
-#         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-#     except cv2.error as e:
-#         print("Error during color conversion:", e)
-#         break
-#
-#     # Detect faces
-#     faces = faceCascade.detectMultiScale(
-#         gray,
-#         scaleFactor=1.1,
-#         minNeighbors=5,
-#         minSize=(30, 30)
-#     )
-#
-#     # Draw rectangle around faces
-#     for (x, y, w, h) in faces:
-#         cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
-#
-#     # Display resulting frame
-#     cv2.imshow('VIDEO', frame)
-#
-#     # Press Esc to exit
-#     if cv2.waitKey(1) == 27:
-#         break
-#
-# # Release video capture and close windows
-# video_capture.release()
-# cv2.destroyAllWindows()
-
 mp_face_mesh = mp.solutions.face_mesh
-face_mesh = mp_face_mesh.FaceMesh()
+face_mesh = mp_face_mesh.FaceMesh(
+    max_num_faces=1,
+    refine_landmarks=True,
+    min_detection_confidence=0.5,
+    min_tracking_confidence=0.5)
 mp_drawing = mp.solutions.drawing_utils
-drawing_spec = mp_drawing.DrawingSpec(thickness=1, circle_radius=1)
 
 video_capture = cv2.VideoCapture(0)
-
 if not video_capture.isOpened():
     raise Exception("Error: Camera not accessible")
 
@@ -69,32 +21,45 @@ try:
             break
 
         rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-
         results = face_mesh.process(rgb_frame)
-
-        face_points = []
 
         if results.multi_face_landmarks:
             for face_landmarks in results.multi_face_landmarks:
-                mp_drawing.draw_landmarks(
-                    image=frame,
-                    landmark_list=face_landmarks,
-                    connections=mp_face_mesh.FACEMESH_TESSELATION,
-                    landmark_drawing_spec=drawing_spec,
-                    connection_drawing_spec=drawing_spec
-                )
+                # Rysowanie twarzy
+                features = [
+                    (mp_face_mesh.FACEMESH_LEFT_EYE, (0, 255, 0)),
+                    (mp_face_mesh.FACEMESH_RIGHT_EYE, (0, 255, 0)),
+                    (mp_face_mesh.FACEMESH_LEFT_EYEBROW, (0, 255, 255)),
+                    (mp_face_mesh.FACEMESH_RIGHT_EYEBROW, (0, 255, 255)),
+                    (mp_face_mesh.FACEMESH_LIPS, (0, 0, 255)),
+                    (mp_face_mesh.FACEMESH_FACE_OVAL, (0, 255, 0))
+                ]
+                for feature, color in features:
+                    mp_drawing.draw_landmarks(
+                        image=frame,
+                        landmark_list=face_landmarks,
+                        connections=feature,
+                        connection_drawing_spec=mp_drawing.DrawingSpec(color=color, thickness=1),
+                        landmark_drawing_spec=None
+                    )
 
-                for id, lm in enumerate(face_landmarks.landmark):
-                    x, y = int(lm.x * frame.shape[1]), int(
-                        lm.y * frame.shape[0])
+                # Rysowanie źrenic
+                left_pupil_point = face_landmarks.landmark[468]
+                right_pupil_point = face_landmarks.landmark[473]
+                left_pupil_coords = (int(left_pupil_point.x * frame.shape[1]), int(left_pupil_point.y * frame.shape[0]))
+                right_pupil_coords = (int(right_pupil_point.x * frame.shape[1]), int(right_pupil_point.y * frame.shape[0]))
 
-                    # OPCJONALNIE WYŚWIETLENIE PUNKTÓW
-                    # cv2.putText(frame, f'{id}', (x, y),
-                    #             cv2.FONT_HERSHEY_SIMPLEX, 0.2, (0, 255, 0), 1)
+                cv2.circle(frame, left_pupil_coords, 3, (255, 0, 255), -1)
+                cv2.circle(frame, right_pupil_coords, 3, (255, 0, 255), -1)
 
-                    face_points.append((id, x, y))
-
-        print(face_points)
+                # Rysowanie nosa
+                indexes_nose = [4, 6, 168, 5, 195, 4, 197]
+                for i in range(len(indexes_nose) - 1):
+                    pt1 = face_landmarks.landmark[indexes_nose[i]]
+                    pt2 = face_landmarks.landmark[indexes_nose[i + 1]]
+                    x1, y1 = int(pt1.x * frame.shape[1]), int(pt1.y * frame.shape[0])
+                    x2, y2 = int(pt2.x * frame.shape[1]), int(pt2.y * frame.shape[0])
+                    cv2.line(frame, (x1, y1), (x2, y2), (255, 0, 0), 1)
 
         cv2.imshow('VIDEO', frame)
 
@@ -103,3 +68,4 @@ try:
 finally:
     video_capture.release()
     cv2.destroyAllWindows()
+
